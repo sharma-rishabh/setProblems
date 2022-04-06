3659d0cf58564e76ea85bf3f6514c026584c00bd
Risabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2022-04-06 15:43:42 +0530

    isEqual is using stacks now

--- groupBy/lib.js
+++ groupBy/lib.js
@@ -1,24 +1,30 @@
 const isArray = Array.isArray;
 
 const isEqual = function (LHS, RHS) {
-  const isOneOfThemPrimitive = !(isArray(LHS) && isArray(RHS));
-  if (isOneOfThemPrimitive) {
+  const isOneOfThemNotArray = !(isArray(LHS) && isArray(RHS));
+  if (isOneOfThemNotArray) {
     return LHS === RHS;
   }
+  const stack = [[LHS, RHS]];
 
-  const lhsArray = LHS, rhsArray = RHS;
+  while (stack.length !== 0) {
+    const bothArrays = stack.pop();
+    const lhsArray = bothArrays[0];
+    const rhsArray = bothArrays[1];
 
-  if (rhsArray.length !== lhsArray.length) {
-    return false;
+    if (lhsArray.length !== rhsArray.length) {
+      return false;
+    }
+
+    for (let index = 0; index < lhsArray.length; index++) {
+      if (isArray(lhsArray[index]) && isArray(rhsArray[index])) {
+        stack.push([lhsArray[index], rhsArray[index]]);
+      } else if (lhsArray[index] !== rhsArray[index]) {
+        return false;
+      }
+    }
   }
-
-  if (rhsArray.length === 0) {
-    return true;
-  }
-
-  const areFirstElementsEqual = isEqual(lhsArray[0], rhsArray[0]);
-  return areFirstElementsEqual && isEqual(lhsArray.slice(1), rhsArray.slice(1));
-
+  return true;
 };
 
 exports.isEqual = isEqual;
